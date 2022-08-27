dd180db3e454b09452ead90578d96be85b1c0944
Price Smith <psmith.code@gmail.com>
2022-08-26 23:22:39 -0400

    experimenting

---
+++ snafu-rs/target/debug/deps/snafu_rs-07034f7e62b1831d.d
@@ -0,0 +1,5 @@
+/Users/sysadmin/git/opensociety/tooling/snafu/OpenSocietyPlatform/snafu/snafu-rs/target/debug/deps/snafu_rs-07034f7e62b1831d.rmeta: src/lib.rs
+
+/Users/sysadmin/git/opensociety/tooling/snafu/OpenSocietyPlatform/snafu/snafu-rs/target/debug/deps/snafu_rs-07034f7e62b1831d.d: src/lib.rs
+
+src/lib.rs:
---
+++ snafu-rs/target/debug/deps/snafu_rs-10ce6762ecd9db62.d
@@ -0,0 +1,5 @@
+/Users/sysadmin/git/opensociety/tooling/snafu/OpenSocietyPlatform/snafu/snafu-rs/target/debug/deps/snafu_rs-10ce6762ecd9db62.rmeta: src/lib.rs
+
+/Users/sysadmin/git/opensociety/tooling/snafu/OpenSocietyPlatform/snafu/snafu-rs/target/debug/deps/snafu_rs-10ce6762ecd9db62.d: src/lib.rs
+
+src/lib.rs:
